422b6c83e973448a3d337ae401aace9e0a8747b8
wangzheng <540964791@qq.com>
2020-04-08 17:30:18 +0800

    httpclient 模拟请求

--- src/main/java/org/jetlinks/demo/openapi/Demo.java
+++ src/main/java/org/jetlinks/demo/openapi/Demo.java
@@ -1,11 +1,37 @@
 package org.jetlinks.demo.openapi;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * @author wangzheng
  * @see
  * @since 1.0
  */
 public class Demo {
+
+    private String clientId;
+
+
+    public Object get(String pathValue) {
+        CloseableHttpClient client = HttpClientBuilder.create()
+                .build();
+
+        return null;
+    }
+
+    public Object delete(String pathValue) {
+        return null;
+    }
+
+    public Object post(Map<String, Object> param) {
+
+        return null;
+    }
 
 
 }
