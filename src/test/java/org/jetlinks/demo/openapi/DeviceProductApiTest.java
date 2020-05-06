7c457cc7508716fa3651bfa3981b8420c5d63852
wangzheng <540964791@qq.com>
2020-05-06 17:23:56 +0800

    增加按设备型号获取数据API测试类

---
+++ src/test/java/org/jetlinks/demo/openapi/DeviceProductApiTest.java
@@ -0,0 +1,49 @@
+package org.jetlinks.demo.openapi;
+
+import org.jetlinks.demo.openapi.util.Utils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author wangzheng
+ * @see
+ * @since 1.0
+ */
+public class DeviceProductApiTest {
+
+    private static final String base_url = "http://localhost:8844/api/v1/product";
+    private static final String productId = "1236859833832701952";
+
+    /**
+     * 根据产品ID和动态查询参数查询设备相关数据测试
+     */
+    @Test
+    void invokeFunctionTest() {
+        String url = base_url + "/" + productId + "/log/_query/";
+
+        System.out.println(url);
+        HttpRequest request = new SimpleHttpRequest(url);
+        String body = "{\n" +
+                "\t\"pageSize\": 25,\n" +
+                "\t\"pageIndex\": 0,\n" +
+                "\t\"terms\": [{\n" +
+                "\t\t\"column\": \"deviceId\",\n" +
+                "\t\t\"value\": \"test001\"\n" +
+                "\t}]\n" +
+                "}";
+
+        request.headers(HeaderUtils.createHeadersOfJsonString(body));
+        System.out.println("Headers:===========>" + HeaderUtils.createHeadersOfJsonString(body));
+        request.requestBody(body);
+
+        try {
+            Response response = request.post();
+            Map<String, String> result = Utils.queryStringToMap(new String(response.asBytes(), "utf8"), "utf8");
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/org/jetlinks/demo/openapi/TokenRequest.java
+++ src/test/java/org/jetlinks/demo/openapi/TokenRequest.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * @see
  * @since 1.0
  */
-public class TokenRequestToken {
+public class TokenRequest {
 
     /**
      * 申请token测试
