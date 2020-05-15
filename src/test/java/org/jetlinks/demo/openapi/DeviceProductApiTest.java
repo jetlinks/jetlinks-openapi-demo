9b18eabce79c84618c307c0480444ef374c9eaa9
wangzheng <540964791@qq.com>
2020-05-15 17:50:42 +0800

    更新根据设备型号查询测试

--- src/test/java/org/jetlinks/demo/openapi/DeviceProductApiTest.java
+++ src/test/java/org/jetlinks/demo/openapi/DeviceProductApiTest.java
@@ -20,8 +20,71 @@ public class DeviceProductApiTest {
      * 根据产品ID和动态查询参数查询设备相关数据测试
      */
     @Test
-    void invokeFunctionTest() {
+    void queryDeviceLogTest() {
         String url = base_url + "/" + productId + "/log/_query/";
+
+        System.out.println(url);
+        HttpRequest request = new SimpleHttpRequest(url);
+        String body = "{\n" +
+                "\t\"pageSize\": 25,\n" +
+                "\t\"pageIndex\": 0,\n" +
+                "\t\"terms\": [{\n" +
+                "\t\t\"column\": \"deviceId\",\n" +
+                "\t\t\"value\": \"test001\"\n" +
+                "\t}]\n" +
+                "}";
+
+        request.headers(HeaderUtils.createHeadersOfJsonString(body));
+        System.out.println("Headers:===========>" + HeaderUtils.createHeadersOfJsonString(body));
+        request.requestBody(body);
+
+        try {
+            Response response = request.post();
+            Map<String, String> result = Utils.queryStringToMap(new String(response.asBytes(), "utf8"), "utf8");
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 分页查询设备属性测试
+     */
+    @Test
+    void queryDevicePropertiesTest() {
+        String url = base_url + "/" + productId + "/properties/_query/";
+
+        System.out.println(url);
+        HttpRequest request = new SimpleHttpRequest(url);
+        String body = "{\n" +
+                "\t\"pageSize\": 25,\n" +
+                "\t\"pageIndex\": 0,\n" +
+                "\t\"terms\": [{\n" +
+                "\t\t\"column\": \"property\",\n" +
+                "\t\t\"value\": \"temperature\"\n" +
+                "\t}]\n" +
+                "}";
+
+        request.headers(HeaderUtils.createHeadersOfJsonString(body));
+        System.out.println("Headers:===========>" + HeaderUtils.createHeadersOfJsonString(body));
+        request.requestBody(body);
+
+        try {
+            Response response = request.post();
+            Map<String, String> result = Utils.queryStringToMap(new String(response.asBytes(), "utf8"), "utf8");
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 查询设备事件测试
+     */
+    @Test
+    void queryDeviceEventsTest() {
+        String eventId = "fire_alarm";
+        String url = base_url + "/" + productId + "/event/" + eventId + "/_query/";
 
         System.out.println(url);
         HttpRequest request = new SimpleHttpRequest(url);
